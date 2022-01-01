35ed3b5d9a495303af76c93394d9c875cc17b95a
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-01-02 00:46:06 +0800

    Update main.js
    Refactor variable name to make it more meaningful

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,24 @@
 // Grid Collapsable Menu (Mobile View)
 const navToggle = (toggleID, navID) => {
     const toggle = document.getElementById(toggleID);
-    const nav = document.getElementById(navID);
+    const navMenu = document.getElementById(navID);
 
     // Proceed when elements exists
-    if (toggle && nav) {
+    if (toggle && navMenu) {
         toggle.addEventListener('click', () => {
             // Add/remove the show__menu class to div.nav__menu
-            nav.classList.toggle('show__menu');
+            navMenu.classList.toggle('show__menu');
         })
     }
 }
 
 // Collapse the menu upon clicking the nav__link (Mobile View)
 const navItemCollapse = (navItemsClass, navID) => {
-    const links = document.querySelectorAll(navItemsClass);
-    const nav = document.getElementById(navID);
+    const navItems = document.querySelectorAll(navItemsClass);
+    const navMenu = document.getElementById(navID);
 
-    links.forEach(link => link.addEventListener('click', () => {
-        nav.classList.remove('show__menu');
+    navItems.forEach(item => item.addEventListener('click', () => {
+        navMenu.classList.remove('show__menu');
     }))
 }
 
